df078b27b9d00737d2fa14d248cfd339cc9734fb
Xabier Fernandez <xabier.fernandez@outlook.com>
2021-06-08 22:04:29 +0200

    update

--- createUserAccountRandomPassword.sh
+++ createUserAccountRandomPassword.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-if [[ "${UID}" -ne 0]]
+#Create user account
+if [[ "${UID}" -ne 0 ]]
 then
   echo "You must log on as root or use sudo commad!"
   exit 1
 fi
-#Create user account
 #Ask for user name 
 read -p 'Enter the username to create: ' USER_NAME
 #Ask for real name
 read -p 'Enter the name of the person who this account is for: ' COMMENT
-#Ask for password
-read -p 'Enter the password to use for the account: ' PASSWORD
+#Generate random password
+SYMBOLS='!@#$%^&*()_-+='
+PASSWORD=$(date +%s%N${SYMBOLS} | sha512sum | head -c48)
+echo "Copy this password in a secure place: ${PASSWORD}"
 #Create user
 useradd -c "${COMMENT}" -m ${USER_NAME}
 #Set password for user
 echo "${USER_NAME}:${PASSWORD}" | chpasswd 
 #Force password change on first login
 passwd -e ${USER_NAME}
+
